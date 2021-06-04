cfe4d3cca25c61d8a6ea404fcec6a57c4c287363
30703070 <suhari123@mail.ru>
2021-06-04 13:31:46 +0300

    my 1 com

---
+++ code.py
@@ -0,0 +1,54 @@
+
+a = input(' enter value')
+print (a)
+my_list = []
+def name_fun( arg1, x ):
+    a = 5
+    my_list = [1, 2, 18, "dfsdf'dnfjd'", a]
+    for element in my_list:
+        print(element )
+    a = a+1
+
+name_fun( 4, 'dfnjdsnfj')
+
+a =1
+if(a == 1 ):
+    print('yes')
+    a+=2
+else:
+    print('no')
+
+
+
+for index in range(2, 10, 3):
+    print( index)
+
+
+a = (1 == 1)
+
+if( a ):
+    print('aaaa')
+
+my_list.append( a)
+
+my_list = my_list + ['fdsf', 'dsfds']
+
+print(my_list)
+
+s = 'aaaa'
+f = 'bbb'
+print( s+" "+ f)
+
+
+a = 5
+
+s = f'a =  {a}'
+print(s)
+
+
+import math
+
+print(math.exp(0))
+import temp
+
+temp.print_value('STRING')
